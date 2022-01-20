af3f457abff7a508509041710ec8c45fa66516d0
kawaguti-39 <89568006+kawaguti-39@users.noreply.github.com>
2022-01-20 13:29:50 +0900

    Update movie.js

--- movie.js
+++ movie.js
@@ -1,7 +1,7 @@
 var v = document.getElementById('video');
 var playsbutton = document.getElementById('plays');
 playsbutton.onclick = playVideo;
-   
+
 function playVideo() {
         //動画を再生
         v.play();
