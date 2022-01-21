da7caa0013c0aabbd42542b4ca22bf851f4f8bde
kawaguti-39 <89568006+kawaguti-39@users.noreply.github.com>
2022-01-21 18:52:35 +0900

    Update movie.js

--- movie.js
+++ movie.js
@@ -1,8 +1,34 @@
-function playVideo() {
-        //動画を再生
-        v.play();
-    }
+// function playVideo() {
+//         //動画を再生
+//         v.play();
+//     }
 
-var v = document.getElementById('video');
-var playsbutton = document.getElementById('plays');
-playsbutton.onclick = playVideo;
+// var v = document.getElementById('video');
+// var playsbutton = document.getElementById('plays');
+// playsbutton.onclick = playVideo;
+
+document.addEventListener("DOMContentLoaded", function(event) {
+      var scene = document.querySelector("a-scene");
+      var video = document.getElementById("load_movie");
+
+      if (scene.hasLoaded) {
+        run();
+      } else {
+        scene.addEventListener("loaded", run);
+      }
+
+      function run () {
+        scene.querySelector("#playButton").addEventListener("click", function(e){
+              video.play();
+          });
+      }
+
+    })
+
+    AFRAME.registerComponent('click-stop', {
+    init: function () {
+      this.el.addEventListener('click', function (event) {
+        document.getElementById("load_movie").pause();
+      });
+    }
+  });
