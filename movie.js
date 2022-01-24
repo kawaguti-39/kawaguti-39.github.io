d925e60c7bea8b2721cdc53d07baa63ea77811a6
kawaguti-39 <89568006+kawaguti-39@users.noreply.github.com>
2022-01-24 16:02:42 +0900

    Add files via upload

--- movie.js
+++ movie.js
@@ -1,34 +1,15 @@
 // function playVideo() {
-//         //動画を再生
-//         v.play();
-//     }
+//     //動画を再生
+//     v.play();
+// }
 
 // var v = document.getElementById('video');
 // var playsbutton = document.getElementById('plays');
 // playsbutton.onclick = playVideo;
 
-document.addEventListener("DOMContentLoaded", function(event) {
-      var scene = document.querySelector("a-scene");
-      var video = document.getElementById("load_movie");
-
-      if (scene.hasLoaded) {
-        run();
-      } else {
-        scene.addEventListener("loaded", run);
-      }
-
-      function run () {
-        scene.querySelector("#playButton").addEventListener("click", function(e){
-              video.play();
-          });
-      }
-
-    })
-
-    AFRAME.registerComponent('click-stop', {
-    init: function () {
-      this.el.addEventListener('click', function (event) {
-        document.getElementById("load_movie").pause();
-      });
-    }
-  });
+<script>
+window.addEventListener('click', function () {
+var v = document.querySelector('#load_movie');
+v.play();
+});
+</script>
